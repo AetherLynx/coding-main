46821ab9b38aba1c9e801647a129c564de86231c
Lynx <81195994+AetherLynx@users.noreply.github.com>
2023-11-30 08:58:11 -0500

    pailina

---
+++ ACTIVIDAD2_CICLICAS/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>ACTIVIDAD :3</title>
+</head>
+<body>
+    <div class="innerbody">
+    <h2>Registro de ventas</h2>
+    </div><br>
+    <div class="rowth">
+        <div class="innerbody" style="height: 500px;">
+            <form method="post" action="index.php">
+                <h4>Ventas del Lunes</h4>
+                <input type="number" name="ventalun">
+                <h4>Ventas del Martes</h4>
+                <input type="number" name="ventamar">
+                <h4>Ventas del Miercoles</h4>
+                <input type="number" name="ventamie">
+                <h4>Ventas del Jueves</h4>
+                <input type="number" name="ventajue">
+                <h4>Ventas del Viernes</h4>
+                <input type="number" name="ventavie">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
